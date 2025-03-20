761466f7c408288966f885e20198c32bbf263aab
Tobias Juarez Alvarez <tobiasjuarezalvarez152@gmail.com>
2025-03-20 15:48:13 -0300

    Se reduce el acceso a las listas mediante indices

--- Questions.py
+++ Questions.py
@@ -29,28 +29,29 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+#Se escoge 3 preguntas de manera aleaotoria (Las preguntas pueden repertirse)
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+# El usuario deberá contestar 3 preguntas
+for question, answers, correct_answer in questions_to_ask:
+    print(question)
+    
+    for i, options in enumerate(answers):
+        print(f"{i+1}. {options}")
+
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):     
         user_answer = (input("Respuesta: ")) 
 
-        if not user_answer.isdigit() or ((int(user_answer)-1) < 0) or ((int(user_answer)-1) >= len(answers[question_index])):
+        if not user_answer.isdigit() or ((int(user_answer)-1) < 0) or ((int(user_answer)-1) >= len(answers)):
             print("Respuesta no valida")
             sys.exit(1)
 
         user_answer = int(user_answer) -1
 
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer:
             score += 1
             print("¡Correcto!")
             break
@@ -60,7 +61,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(answers[correct_answer])
 
     # Se imprime un blanco al final de la pregunta
     print()
